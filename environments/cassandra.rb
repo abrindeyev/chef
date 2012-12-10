ab8b4bf668dee1ba7fb8bdfce73b351d35137792
Andrey Brindeyev <abrindeyev@griddynamics.com>
2012-12-10 11:29:43 -0800

    Added Cassandra env

---
+++ environments/cassandra.rb
@@ -0,0 +1,12 @@
+name "cassandra"
+description "Apache Cassandra environment"
+default_attributes({
+    "repository" => "http://chef",
+    "java" => {
+        "install_flavor" => "oracle",
+        "jdk_version" => "7",
+        "jdk" => { "7" => { "x86_64" => {
+            "url" => "http://chef/d/jdk-7u7-linux-x64.tar.gz",
+            "checksum" => "3d1c33c832ec278058fcba979b575debe20bb8a309bc39321795d26ee29d9f35"
+        }}}},
+})
