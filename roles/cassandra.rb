1d52a2163c1270b79eb98f2bb2c0d4b745160e2e
Andrey Brindeyev <abrindeyev@griddynamics.com>
2012-12-10 17:46:29 -0800

    Changed JDK from version 7 to 6 for Cassandra

--- roles/cassandra.rb
+++ roles/cassandra.rb
@@ -4,12 +4,11 @@ run_list(
   "recipe[java]"
 )
 default_attributes({
-    "repository" => "http://chef",
     "java" => {
         "install_flavor" => "oracle",
-        "jdk_version" => "7",
-        "jdk" => { "7" => { "x86_64" => {
-            "url" => "http://chef/d/jdk-7u7-linux-x64.tar.gz",
-            "checksum" => "3d1c33c832ec278058fcba979b575debe20bb8a309bc39321795d26ee29d9f35"
+        "jdk_version" => "6",
+        "jdk" => { "6" => { "x86_64" => {
+            "url" => "http://chef/d/jdk-6u35-linux-x64.bin",
+            "checksum" => "01dd8c70866ffd6351987bf7cb2331f077539f84d24a9c4581f230be5422a6bb"
         }}}},
 })
