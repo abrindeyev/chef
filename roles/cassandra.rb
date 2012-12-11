76ca8bae6854a753707c9e44f93bb010c5e37e3c
Andrey Brindeyev <abrindeyev@griddynamics.com>
2012-12-11 15:58:41 -0800

    Added simple_cassandra recipe, reformatted java recipe variables

--- roles/cassandra.rb
+++ roles/cassandra.rb
@@ -1,14 +1,20 @@
 name "cassandra"
 description "Simple Cassandra node"
 run_list(
-  "recipe[java]"
+  "recipe[java]",
+  "recipe[simple_cassandra]"
 )
 default_attributes({
-    "java" => {
-        "install_flavor" => "oracle",
-        "jdk_version" => "6",
-        "jdk" => { "6" => { "x86_64" => {
-            "url" => "http://chef/d/jdk-6u35-linux-x64.bin",
-            "checksum" => "01dd8c70866ffd6351987bf7cb2331f077539f84d24a9c4581f230be5422a6bb"
-        }}}},
+  "java" => {
+    "install_flavor" => "oracle",
+    "jdk_version" => "6",
+    "jdk" => {
+      "6" => {
+        "x86_64" => {
+          "url" => "http://chef/d/jdk-6u35-linux-x64.bin",
+          "checksum" => "01dd8c70866ffd6351987bf7cb2331f077539f84d24a9c4581f230be5422a6bb",
+        }
+      }
+    }
+  },
 })
